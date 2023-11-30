28b712dad001c0b8d71409fb9c770309f1899765
Semicile17 <rohitsinghmahant707@gmail.com>
2023-11-30 15:09:59 +0530

    added new links and font

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import { BsArrowUpRight } from "react-icons/bs";
 import { BiLogoInstagram, BiLogoLinkedin, BiLogoYoutube } from "react-icons/bi";
 import { BiLogoFacebook } from "react-icons/bi";
 import bg2 from "../../Images/homeImages/homepage_bg2.png";
+import "../../assets/Exo-VariableFont_wght.ttf";
 const data1 = [
   {
     id: 2,
@@ -44,11 +45,11 @@ const Footer = () => {
       <div className="flex flex-col py-5 border-b-4 md:py-10 md:flex-row md:justify-between">
         <div className="flex items-center justify-evenly ">
           {data1.map((item) => (
-            <p
+            <p style={{ fontFamily: "exo" }}
               className="flex items-center px-3 py-1 mx-0.5 md:mx-2 bg-[#0c2c53] rounded-lg  text-[10px] md:text-xl hover:text-yellow-500"
               key={item.id}
             >
-              <a href={item.link} target="_blank" className="">
+              <a href={item.link} target="_blank" className="" style={{ fontFamily: "exo" }}>
                 {item.name}
               </a>
               <BsArrowUpRight />
@@ -58,11 +59,11 @@ const Footer = () => {
         <div className="flex justify-around mt-5">
           <ul
             className="flex flex-wrap items-center justify-evenly text-[12px] w-48 md:w-96 md:text-2xl "
-            style={{ fontFamily: "sharetech" }}
+            style={{ fontFamily: "exo" }}
           >
             {data2.map((item) => (
-              <li key={item.id} className="px-2 hover:text-yellow-500">
-                <a href={item.link} target="_blank">
+              <li key={item.id} className="px-2 hover:text-yellow-500" style={{ fontFamily: "exo" }}>
+                <a href={item.link} target="_blank" style={{ fontFamily: "exo" }}>
                   {item.name}
                 </a>
               </li>
@@ -71,27 +72,27 @@ const Footer = () => {
         </div>
       </div>
       <div className="py-6">
-        <p className="md:text-xl" style={{ fontFamily: "sharetech" }}>
+        <p className="md:text-xl" style={{ fontFamily: "exo" }}>
           Connect With Us On:
         </p>
         <div className="flex">
           <div>
-            <a href="https://www.linkdin.com">
+            <a href="https://www.linkedin.com/in/sac-gbpiet-791260289/?originalSubdomain=in">
               <BiLogoLinkedin className="text-white md:text-2xl" />
             </a>
           </div>
           <div className="">
-            <a href="https://www.facebook.com">
+            <a href="https://m.facebook.com/p/SAC-Gbpiet-100091933003060/">
               <BiLogoFacebook className="text-white md:text-2xl" />
             </a>
           </div>
           <div className="">
-            <a href="https://www.instagram.com">
+            <a href="https://instagram.com/sacgbpiet?igshid=MzMyNGUyNmU2YQ==">
               <BiLogoInstagram className="text-white md:text-2xl" />
             </a>
           </div>
           <div>
-            <a href="https://www.youtube.com/">
+            <a href="https://www.youtube.com/channel/UC2Dtejxsdmr459qmO2NyQow">
               <BiLogoYoutube className="text-white md:text-2xl" />
             </a>
           </div>
@@ -100,7 +101,7 @@ const Footer = () => {
       <div className="pb-5">
         <p
           className="text-[12px] md:text-xl"
-          style={{ fontFamily: "sharetech" }}
+          style={{ fontFamily: "exo" }}
         >
           @Spandan 2k23.All Right Reserved
         </p>
